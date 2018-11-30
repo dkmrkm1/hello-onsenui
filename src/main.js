b56f6942216cc30f34e54c0e0d4f688b3ade78e1
Duuun <daimura10@gmail.com>
2018-11-30 14:58:17 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+
+import 'font-awesome/css/font-awesome.css'
+import 'onsenui/css/onsenui-core.css'
+import 'onsenui/css/onsen-css-components.css'
+
+import Vue from 'vue'
+import $ons from 'vue-onsenui/esm'
+import * as VOns from './vue-onsen-components'
+import store from './store'
+import App from './App'
+
+Vue.config.productionTip = false
+
+Vue.use($ons)
+Object.values(VOns).forEach(comp => Vue.component(comp.name, comp))
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  render: h => h(App)
+})
